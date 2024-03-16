e44a6ccd3b07247a9a1b0616993f499a87101f62
Amarjeet <amarjeetrajking@gmail.com>
2024-03-16 23:56:58 +0530

    Baisc of React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,14 @@
-
+import Chai from "./chai"
 function App() {
 
   return (
-    <h1>Hello world, learning react with vite | Amarjeect</h1>
+    <> 
+    {/*Fragment is a component that lets you group a list of children elements without creating an extra DOM node  // <></> is the shorthand that we can use instead of React.Fragment*/}
+    <Chai/>
+    <h1>Hii Amarjeet</h1>
+    <p>Take you chai</p>
+    {/* Multiple element return */}
+    </>
   )
 }
 
